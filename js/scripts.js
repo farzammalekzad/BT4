6f2a8bb125c6e2331bacd7a4444d9bcaea5cb80c
Farzam Malekzad <mohammad.malekzad@yahoo.com>
2019-11-03 23:00:32 +0330

    NPM script part 2

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    })
+});
+$(document).ready(function () {
+    $('#reserveButton').click(function () {
+        $('#reserveModal').modal('show');
+    });
+});
+$(document).ready(function () {
+    $('#modalhide').click(function () {
+        $('#reserveModal').modal('hide');
+    });
+});
+$(document).ready(function () {
+    $('#modalCancel').click(function () {
+        $('#reserveModal').modal('hide');
+    });
+});
+$(document).ready(function () {
+    $('#loginButton').click(function () {
+        $('#loginModal').modal('show');
+    });
+});
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
